65f50d7b46b2b59664df5f50034202643e6e4765
Paul Santiago <paulsantiago282@gmail.com>
2023-07-23 22:11:33 -0400

    initial commit for components

--- my-recipe-app/src/app/app.module.ts
+++ my-recipe-app/src/app/app.module.ts
@@ -2,10 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component'
+import {RecipeComponent} from './recipe/recipe.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingEditComponent,
   ],
   imports: [
     BrowserModule
---
+++ my-recipe-app/src/app/recipe/recipe.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-recipe',
+  templateUrl: './recipe.component.html'
+})
+export class RecipeComponent {
+
+}
